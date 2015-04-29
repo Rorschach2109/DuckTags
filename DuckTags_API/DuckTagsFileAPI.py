023bf7cd388af62a8e01e8687e6469b9bcce92cc
azazello <rorschach2109@gmail.com>
2015-04-29 13:55:23 +0200

    Add decorator for getting files from folder

--- DuckTags_API/DuckTagsFileAPI.py
+++ DuckTags_API/DuckTagsFileAPI.py
@@ -1,5 +1,17 @@
 from Src.DuckTagsFileManager import DuckTagsFileManager
 
+import functools
+
+
+def music_files_decorator(get_files_foo):
+
+    @functools.wraps(get_files_foo)
+    def wrapper(file_api, folder_path):
+        files_dict = get_files_foo(file_api, folder_path)
+        return file_api.get_music_files_from_files_dict(files_dict)
+
+    return wrapper
+
 
 class DuckTagsFileAPI(object):
     def __init__(self):
@@ -8,6 +20,7 @@ class DuckTagsFileAPI(object):
             'mp3'
         ]
 
+    @music_files_decorator
     def get_files_dict_from_folder(self, folder_path):
         return self.file_manager.get_files_dict_from_folder(folder_path)
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from DuckTagsView.DuckTagsAppMain import DuckTagsAppMain
+
+
+if __name__ == '__main__':
+    DuckTagsAppMain.run_duck_tags_app()
