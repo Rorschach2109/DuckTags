1835606b71dabdd1ce667a419f58b9b5aaa57818
azazello <rorschach2109@gmail.com>
2015-04-30 13:14:14 +0200

    Add Files Panel on browse folder method

--- DuckTagsView/DuckTagsAppFilesPanel.py
+++ DuckTagsView/DuckTagsAppFilesPanel.py
@@ -1,6 +1,13 @@
+from DuckTags_API.DuckTagsFileAPI import DuckTagsFileAPI
+
 from PySide import QtGui
 
 
 class DuckTagsAppFilesPanel(QtGui.QListWidget):
     def __init__(self, *args, **kwargs):
         super(DuckTagsAppFilesPanel, self).__init__(*args, **kwargs)
+
+        self.file_api = DuckTagsFileAPI()
+
+    def on_browse_folder(self, selected_directory):
+        files_dict = self.file_api.get_files_dict_from_folder(selected_directory)
