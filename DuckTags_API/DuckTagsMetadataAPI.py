2b9860abb35cde01d2c85cd8c65d53509b8bd37f
azazello <rorschach2109@gmail.com>
2015-05-07 13:57:03 +0200

    Fix BUG with missing method argument

--- DuckTags_API/DuckTagsMetadataAPI.py
+++ DuckTags_API/DuckTagsMetadataAPI.py
@@ -11,11 +11,12 @@ class DuckTagsMetadataAPI(object):
     def get_music_files_list_metadata(self, music_files_paths_list):
         return self.metadata_manager.get_music_files_list_metadata(music_files_paths_list)
 
-    def set_music_file_metadata(self, music_file_path, music_metadata_dict, cover_path):
-        return self.metadata_manager.set_music_file_metadata(music_file_path, music_metadata_dict, cover_path)
+    def set_music_file_metadata(self, music_file_path, music_metadata_dict):
+        return self.metadata_manager.set_music_file_metadata(music_file_path, music_metadata_dict)
 
-    def set_music_file_list_metadata(self, music_files_paths_list, music_metadata_dict):
-        return self.metadata_manager.set_music_file_list_metadata(music_files_paths_list, music_metadata_dict)
+    def set_music_file_list_metadata(self, music_files_paths_list, music_metadata_dict, cover_path):
+        return self.metadata_manager.set_music_file_list_metadata(music_files_paths_list, music_metadata_dict,
+                                                                  cover_path)
 
     def set_music_file_metadata_uppercase(self, music_file_path):
         return self.metadata_manager.set_music_file_metadata_uppercase(music_file_path)
